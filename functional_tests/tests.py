fa076140a870841b68b97445f278d81d8fc85fda
Philip Gillespie <philipgillespie91@gmail.com>
2026-01-22 20:18:15 +0000

    Add functional test for multiple users lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -72,3 +72,45 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table("2: Use peacock feathers to make fly")
 
         # Satisfied, she goes back to sleep
+        return None
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new list
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        input_box.send_keys("Buy peacock feathers")
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+
+        # Now a new user, Francis, comes along to the site.
+        ## Delete all cookies to simulate a brand new user session
+        self.browser.delete_all_cookies()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+
+        # Francis starts a new list by entering a new item.
+        # He is less interesting than edith.
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        input_box.send_keys("Buy milk")
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: " + "Buy milk")
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again there is no trace of Edith's list
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+
+        # Satisfied they both go back to sleep
+        return None
